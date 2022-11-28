b74e146193bcb061b66000752b1ac3b3427485b4
asta12 <45568620+asta12@users.noreply.github.com>
2022-11-28 12:08:08 +0100

    Song search (#13)
    * update gitignore to ignore secret config
    
    * SearchSongPresenter with SeachSongView and SearchResultsView in it
    
    * CSS changes, click enter to search, misspelling
    
    * removes a console error
    
    Co-authored-by: asta12 <astaol@kth.se>

---
+++ music-quiz/src/songSource.js
@@ -0,0 +1,21 @@
+import { API_KEY } from "./apiConfig.js"
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': API_KEY,
+		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+	}
+};
+
+function searchSong(searchString, page_offset) {
+    let encodedSearch = encodeURI(searchString) // URL encode search string
+    let perPage = 10 // entries per page
+    let page = page_offset // page offset
+    return fetch(`https://genius-song-lyrics1.p.rapidapi.com/search?q=${encodedSearch}&per_page=${perPage}&page=${page}`, options)
+	        .then(response => response.json())
+	        .catch(err => console.error(err));
+}
+
+
+export { searchSong } 
---
+++ music-quiz/src/views/searchResultsView.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react"
+
+export default
+function SearchResultsView(props) {
+
+    let res = <ul></ul> 
+    if (props.searchResults) {
+        res = <ul>
+                {props.searchResults.response.hits.map((hit, index) => {
+                    return <li key={index}>{hit.result.full_title}</li>
+                })}
+            </ul>
+    }
+
+    return  <div>{ res }</div>
+}
