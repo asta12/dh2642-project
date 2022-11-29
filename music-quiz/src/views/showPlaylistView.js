846babe2d3cd1e263d81c5e23413ca513f7bcabd
asta12 <astaol@kth.se>
2022-11-29 23:32:07 +0100

    creates a user profile presenter

--- music-quiz/src/views/showPlaylistView.js
+++ music-quiz/src/views/showPlaylistView.js
@@ -1,119 +1,62 @@
-import React, { useState } from "react";
+import React from "react";
 import { ListGroup } from "react-bootstrap";
-import { Button, Stack } from "react-bootstrap";
+import { Button, Stack, Image } from "react-bootstrap";
 import down from "../images/down.png";
 import up from "../images/up.png";
 
-// testdata: this should be retrieved from props
-let playlists = [
-  {
-    name: "Christmas Songs",
-    songs: [
-      {
-        title: "Last Christmas",
-        artist: "Wham!",
-      },
-      {
-        title: "All I Want For Christmas Is you",
-        artist: "Mairah Carey",
-      },
-      {
-        title: "Run Rudolph Run",
-        artist: "Chuck Berry",
-      },
-    ],
-  },
-  {
-    name: "80's",
-    songs: [
-      {
-        title: "Take On Me",
-        artist: "A-ha",
-      },
-      {
-        title: "Africa",
-        artist: "Toto",
-      },
-      {
-        title: "Never Gonna Give You Up",
-        artist: "Rick Astley",
-      },
-    ],
-  },
-  {
-    name: "Queen",
-    songs: [
-      {
-        title: "Bohemian Rhapsody",
-        artist: "Queen",
-      },
-      {
-        title: "Another One Bites The Dust",
-        artist: "Queen",
-      },
-      {
-        title: "We Will Rock You",
-        artist: "Queen",
-      },
-    ],
-  },
-];
-
 export default function ShowPlaylistView(props) {
-  const [current, updateCurrent] = useState(-1);
 
-  function expand(i) {
-    if (current === i) {
-      updateCurrent(-1);
-    } else {
-      updateCurrent(i);
-    }
-  }
-
-  return (
-    <div>
+  return (  
+    <div> 
       <h2>Playlists:</h2>
       <ListGroup>
-        {playlists.map((playlist, index) => {
+        {props.playlists.map((playlist, index) => {
           let songs = <></>;
-          let img = (
+          let expandImg = (
             <img
               src={up}
               alt="add item"
               width="25"
               onClick={(e) => {
-                expand(index);
+                props.expand(index);
               }}
             />
           );
-          if (index === current) {
+          if (props.expanding[index]) {
             songs = (
               <ListGroup>
                 {playlist.songs.map((song, songIndex) => {
                   return (
                     <ListGroup.Item key={songIndex + 100}>
-                      {song.title} - {song.artist}
+                      <Image
+                        src={song.img_src}
+                        thumbnail
+                        className="me-3"
+                        width="70"
+                        height="70"
+                      />
+                      {song.title} - {song.artist_names}
                     </ListGroup.Item>
                   );
                 })}
               </ListGroup>
             );
-            img = (
+            expandImg = (
               <img
                 src={down}
                 alt="add item"
                 width="25"
                 onClick={(e) => {
-                  expand(index);
+                  props.expand(index);
                 }}
               />
             );
           }
           return (
-            <ListGroup.Item>
+            <ListGroup.Item key={index}>
               <Stack direction="horizontal">
                 <div style={{ display: "flex" }}>
-                  {img}
+                  {expandImg}
                   <h5>{playlist.name}</h5>
                 </div>
                 <div className="ms-auto">
@@ -122,7 +65,8 @@ export default function ShowPlaylistView(props) {
                     onClick={(e) => {
                       console.log("edit " + playlist.name);
                     }}
-                    variant="outline-primary">
+                    variant="outline-primary"
+                  >
                     Edit
                   </Button>
                 </div>
