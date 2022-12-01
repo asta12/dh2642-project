31a12bad875fac337964647f0f6da392b513718d
Marcus Alevärn <marcus.alevarn@gmail.com>
2022-12-01 22:57:43 +0100

    add function getSongLyrics

--- music-quiz/src/songSource.js
+++ music-quiz/src/songSource.js
@@ -1,11 +1,12 @@
 import { API_KEY } from "./apiConfig.js"
+import { getLyrics } from 'genius-lyrics-api';
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': API_KEY,
-		'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+    }
 };
 
 function searchSong(searchString, page_offset) {
@@ -13,9 +14,16 @@ function searchSong(searchString, page_offset) {
     let perPage = 5 // entries per page
     let page = page_offset // page offset
     return fetch(`https://genius-song-lyrics1.p.rapidapi.com/search?q=${encodedSearch}&per_page=${perPage}&page=${page}`, options)
-	        .then(response => response.json())
-	        .catch(err => console.error(err));
+        .then(response => response.json())
+        .catch(err => console.error(err));
 }
 
+function getSongLyrics(songID) {
+    return fetch(`https://genius-song-lyrics1.p.rapidapi.com/songs/${songID}`, options)
+        .then(response => response.json())
+        .then(json => json.response.song.url)
+        .then(url => getLyrics(url))
+        .catch(err => console.error(err));
+}
 
-export { searchSong } 
+export { searchSong, getSongLyrics } 
