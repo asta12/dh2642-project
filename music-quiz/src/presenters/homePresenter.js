26289a095630efa7f83bbd180cf9458c6c9a4d20
kanova98 <43655561+kanova98@users.noreply.github.com>
2022-12-07 18:10:42 +0100

    Home presenter/view (#49)
    * Add simple homePresenter/view
    
    * add formatting

---
+++ music-quiz/src/presenters/homePresenter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeView from "../views/homeView";
+
+function HomePresenter(props) {
+  return <HomeView link={props.model.currentUser ? "/play" : "/register"} />;
+}
+
+export default HomePresenter;
