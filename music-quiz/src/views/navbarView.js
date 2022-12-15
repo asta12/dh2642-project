0386e0bb6b6af8d9e0980ab33585ea647bf40271
Anna Skantz <askantz@kth.se>
2022-12-15 17:39:52 +0100

    "Fixes homepage and general design"
    Co-authored-by: asta12 <asta12@users.noreply.github.com>

--- music-quiz/src/views/navbarView.js
+++ music-quiz/src/views/navbarView.js
@@ -1,31 +1,62 @@
 import React from "react";
-import { Nav, Navbar, Container} from "react-bootstrap";
+import { Nav, Navbar, Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import head from "../images/robot_head.png";
 
 export default function navbarView(props) {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg">
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      style={{ borderRadius: "10px", height: "45px" }}
+    >
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" key="home">
+          <Nav>
+            <Nav.Link as={Link} to="/" key="home" style={{ color: "white" }}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/profile" key="profile">
+            <Nav.Link
+              as={Link}
+              to="/profile"
+              key="profile"
+              style={{ color: "white" }}
+            >
               Profile
             </Nav.Link>
-            <Nav.Link as={Link} to="/about" key="about">
+            <Nav.Link
+              as={Link}
+              to="/about"
+              key="about"
+              style={{ color: "white" }}
+            >
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="/play" key="play">
+            <Nav.Link
+              as={Link}
+              to="/play"
+              key="play"
+              style={{ color: "white" }}
+            >
               Play
             </Nav.Link>
             {(!props.loggedInUser && [
-              <Nav.Link as={Link} to="/login" key="login">
+              <Nav.Link
+                as={Link}
+                to="/login"
+                key="login"
+                style={{ color: "white" }}
+              >
                 Log in
               </Nav.Link>,
-              <Nav.Link as={Link} to="/register" key="register">
+              <Nav.Link
+                as={Link}
+                to="/register"
+                key="register"
+                style={{ color: "white" }}
+              >
                 Register
               </Nav.Link>,
             ]) || (
@@ -36,10 +67,16 @@ export default function navbarView(props) {
                     props.handleLogout();
                   }}
                   key="logout"
+                  style={{ color: "white" }}
                 >
                   Logout
                 </Nav.Link>
-                <Nav.Link as={Link} to="/profile/add-friend" key="add-friend">
+                <Nav.Link
+                  as={Link}
+                  to="/profile/add-friend"
+                  key="add-friend"
+                  style={{ color: "white" }}
+                >
                   Add friend
                 </Nav.Link>
               </>
