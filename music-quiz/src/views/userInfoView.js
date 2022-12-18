714073cfff6a2b5d63c7abd4dabd61a8bba25221
kanova98 <43655561+kanova98@users.noreply.github.com>
2022-12-18 12:30:17 +0100

    Add changes to profile and profile pic in navbar (#95)

--- music-quiz/src/views/userInfoView.js
+++ music-quiz/src/views/userInfoView.js
@@ -1,23 +1,25 @@
-import { Form, Image, Stack } from "react-bootstrap";
+import { Form, Image, Stack, Card } from "react-bootstrap";
 import { getImgSrc } from "../settings/profileSettings";
 
 function UserInfo(props) {
   let img = getImgSrc(props.username);
   return (
-    <div>
-      <Stack direction="horizontal">
-        <h2>Profile</h2>
-        <Image src={img} width="70" height="70" className="me-3" />{" "}
-      </Stack>
+    <div className="d-flex" style={{ justifyContent: "center" }}>
+      <Card style={{ padding: "10px", width: "25rem" }}>
+        <Stack direction="horizontal">
+          <h2>Profile</h2>
+          <Image className="ms-auto" src={img} width="70" height="70" />{" "}
+        </Stack>
 
-      <Form.Group className="mb-3">
-        <Form.Label>Username</Form.Label>
-        <Form.Control placeholder={props.username} disabled />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Email</Form.Label>
-        <Form.Control placeholder={props.email} disabled />
-      </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Username</Form.Label>
+          <Form.Control placeholder={props.username} disabled />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control placeholder={props.email} disabled />
+        </Form.Group>
+      </Card>
     </div>
   );
 }
